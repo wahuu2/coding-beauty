6796a1eee27215849f8614ad8b460ec890b97e58
wahuu2 <lilywahu1@gmail.com>
2026-07-16 22:07:59 +0300

    Carouse 2

---
+++ Carousels/Carousel 2/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+  speed: 750,
+  parallax: true,
+  autoplay: false,
+  mousewheel: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
