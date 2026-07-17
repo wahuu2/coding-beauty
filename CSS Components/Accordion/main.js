77b67c79911c2242de22da01a1e2c85eba590459
wahuu2 <lilywahu1@gmail.com>
2026-07-17 07:26:14 +0300

    Accordion 1

--- CSS Components/Accordion/main.js
+++
@@ -1,8 +0,0 @@
-const accordion = document.querySelector(".image-accordion");
-const items = accordion.querySelectorAll(".image-accordion-item");
-
-accordion.addEventListener("click", (e) => {
-  const item = e.target.closest(".image-accordion-item");
-  if (!item) return;
-  items.forEach((el) => el.classList.toggle("active", el === item));
-});
